cc6ea0dc68496d8d41553ae9d1bcb237e7061b85
Ngare <ngaremaina4@gmail.com>
2023-09-10 17:47:31 +0300

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -24,9 +24,6 @@ function App() {
     fetchingProducts()
   },[])
 
-  console.log(products)
-  
-
   return (
     <div className="App">
       <NavBar />
--- client/src/components/DetailsPage.js
+++ client/src/components/DetailsPage.js
@@ -3,17 +3,17 @@ import { useParams, Link, useSearchParams } from "react-router-dom";
 import DetailsItem from "./DetailsItem";
 
 const DetailsPage = ({getProduct, product}) => {
-    var category = ""
+    var group = ""
     var rating = 0
     const {name} = useParams()
     getProduct(name)
 
-    product.category?.map(category => {category = category.name })
+    product.category?.map(cat => {group = cat.name })
     product.rating?.map(rate => {rating = rate.rate})
     
     
     return(
-        <DetailsItem key = {product.id} name = {product.name} image = {product.image} description={product.description} price={product.price} category={category} rate = {rating} />
+        <DetailsItem key = {product.id} name = {product.name} image = {product.image} description={product.description} price={product.price} category={group} rate = {rating} />
     )
 
 }
