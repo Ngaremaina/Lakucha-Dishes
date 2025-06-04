db27c5cd0d10ebe34b8c2a2350e8a533bb5c38d7
Ngare Maina <45742522+Ngaremaina@users.noreply.github.com>
2025-06-04 15:00:18 +0300

    ✨ Vercel Analytics Integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import './App.css';
 import { Authentication } from './context/Authentication';
 import { GlobalProvider } from './context/GlobalContext';
 import AppRoutes from './routes/Approutes';
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
 
   return (
     <>
+      <Analytics/>
       <Authentication>
         <GlobalProvider>
           <AppRoutes/>
