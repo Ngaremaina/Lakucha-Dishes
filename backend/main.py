ff88b796ab6cf9d7ec24fbade8e2c9266741e96d
Ngaremaina <ngaremaina4@gmail.com>
2026-07-04 22:48:57 +0300

    Updated Backend

--- backend/main.py
+++
@@ -1,9 +0,0 @@
-from server import app, route
-from werkzeug.serving import run_simple
-
-if __name__ == '__main__':
-    run_simple(
-        hostname='localhost',
-        port=5000,
-        application=app
-    )
